58a4cca70051b9ce48a4f90817de5b7e5451137b
mlatysheva <mlatysheva@gmail.com>
2022-12-20 22:35:34 +0100

    fix: minor bug

--- src/compress/decompress.js
+++ src/compress/decompress.js
@@ -8,20 +8,29 @@ import { consoleColors } from '../utils/consoleColors.js';
 export const decompress = async (fileToDecompress, newDestination) => {
   try {
     if (fileToDecompress.slice(-3) !== '.br') {
-      invalidInputMessage(`${fileToDecompress} is not a valid compressed file. Specify a file with a valid extention ".br"`);
+      invalidInputMessage(
+        `${fileToDecompress} is not a valid compressed file. Specify a file with a valid extention ".br"`
+      );
       cwdMessage();
     } else {
       const filename = fileToDecompress.slice(0, -3).replace(/^.*[\\\/]/, '');
-      const paths = await checkPaths(fileToDecompress, newDestination, filename);
+      const paths = await checkPaths(
+        fileToDecompress,
+        newDestination,
+        filename
+      );
       if (paths) {
-        const { absolutePath, newAbsolutePath } = paths;       
+        const { absolutePath, newAbsolutePath } = paths;
         const fileToDecompress = createReadStream(absolutePath);
         const writableStream = createWriteStream(newAbsolutePath);
         const brotli = zlib.createBrotliDecompress();
 
         fileToDecompress.pipe(brotli).pipe(writableStream);
 
-        console.log(consoleColors.cyan, `The file ${fileToDecompress} was successfully decompressed to ${newAbsolutePath}.`);
+        console.log(
+          consoleColors.cyan,
+          `The file was successfully decompressed to ${newAbsolutePath}.`
+        );
         cwdMessage();
       } else {
         invalidInputMessage();
